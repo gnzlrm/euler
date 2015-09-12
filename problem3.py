38a6ee40b70db4e4cb61efe8700f38d68ed605ac
gnzlrm <gnzl.rm@gmail.com>
2015-09-12 10:32:16 -0300

    Added problems 3, 5 and 6. Rename 1 and 2

---
+++ problem3.py
@@ -0,0 +1,40 @@
+# Code by Gonzalo Rolon Morinelli, Sep 9 2015
+# The following code was originally written as a solution for Project Euler's Problem# 3
+# For more information, goto: https://projecteuler.net/problem=3
+
+import math
+
+def get_primes_lt(n):
+    """
+    Return a list with all the prime numbers lesser than n.
+    Custom implementation of Eratosthenes's sieve.
+    """
+    comp_set = set([])
+    prime_list = []
+    number = 2
+    while number < n:
+        if number not in comp_set:
+            prime_list.append(number)
+            multiple = number * number
+            while multiple < n:
+                comp_set.add(multiple)
+                multiple += number
+        number += 1
+    return prime_list
+
+def find_largest_prime_factor(n):
+    """
+    Return the largest prime factor of the input composite number n.
+    If n is unusual, it returns the second largest prime factor instead.
+    """
+    primes = get_primes_lt(math.sqrt(n) + 1)
+    primes.reverse()
+    for prime in primes:
+        if n % prime == 0:
+            return prime
+    return -1
+
+
+if __name__ == '__main__':
+    # Problem solution
+    print find_largest_prime_factor(600851475143)
