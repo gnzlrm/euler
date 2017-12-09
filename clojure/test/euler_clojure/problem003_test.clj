760213bc48e6f605a227f5bc336a4e12fbb8ae6d
gnzlrm <gnzl.rm@gmail.com>
2017-12-09 11:54:50 -0300

    Added problem003 test.

--- clojure/test/euler_clojure/problem003_test.clj
+++ clojure/test/euler_clojure/problem003_test.clj
@@ -2,4 +2,11 @@
   (:use [euler-clojure.problem003]
         [clojure.test]))
 
-(t/deftest inverse-primes<)
+(deftest t-inverse-primes<
+  []
+  (is (= '(7 5 3 2) (inverse-primes< 10))))
+
+(deftest t-largest-prime-factor
+  []
+  (is (= 29 (largest-prime-factor 13195)))
+  (is (= 6857 (largest-prime-factor 600851475143))))
